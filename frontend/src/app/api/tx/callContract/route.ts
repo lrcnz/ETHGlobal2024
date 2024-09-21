0ff8bb7c354f5ffe6f4e8fc1da57ec1dd67f556f
qwer951123 <qiuhao951@gmail.com>
2024-09-21 17:13:37 +0800

    fix : fix build error

--- frontend/src/app/api/tx/callContract/route.ts
+++ frontend/src/app/api/tx/callContract/route.ts
@@ -1,28 +1,35 @@
 import { userControlledWalletsClient } from "@/lib/user-controller-sdk";
 
-export async function POST(req: Response) {
+export async function POST(req: Request) {
   const body = await req.json();
   const headers = req.headers;
 
-  // circle-sdk: create user transaction contract execution challenge
-  const response = await userControlledWalletsClient.createUserTransactionContractExecutionChallenge({
-    userToken: headers.get('token') as string,
-    abiFunctionSignature: body.abiFunctionSignature,
-    abiParameters: body.abiParameters,
-    amount: body.amount,
-    contractAddress: body.contractAddress,
-    idempotencyKey: body.idempotencyKey,
-    refId: body.refId,
-    walletId: body.walletId,
-    fee: {
-      type: "level",
-      config: {
-        feeLevel: body.feeLevel
+  try {
+    // circle-sdk: create user transaction contract execution challenge
+    const response = await userControlledWalletsClient.createUserTransactionContractExecutionChallenge({
+      userToken: headers.get('token') as string,
+      abiFunctionSignature: body.abiFunctionSignature,
+      abiParameters: body.abiParameters,
+      amount: body.amount,
+      contractAddress: body.contractAddress,
+      idempotencyKey: body.idempotencyKey,
+      refId: body.refId,
+      walletId: body.walletId,
+      fee: {
+        type: "level",
+        config: {
+          feeLevel: body.feeLevel
+        }
       }
-    }
-  });
+    });
 
-  return Response.json({ result: "success", data: {
-    challengeId: response.data?.challengeId
-  }}, { status: 200 });
+    return Response.json({
+      result: "success", data: {
+        challengeId: response.data?.challengeId
+      }
+    }, { status: 200 });
+
+  } catch (e) {
+    return Response.json({ result: "error", message: e }, { status: 400 });
+  }
 }
