7838add63495f75216392010e1e5dc30ad0c4dcb
qwer951123 <qiuhao951@gmail.com>
2024-10-11 10:27:32 +0800

    feat: add next circult in withdraw ustb

--- frontend/src/lib/actions/actions/superstate-sell.tsx
+++ frontend/src/lib/actions/actions/superstate-sell.tsx
@@ -12,7 +12,7 @@ export class SuperstateSellAction implements Action {
   }
 
   public getNextCircuit(_data: CircuitData): Circuit[] {
-    return [];
+    return [new Circuit({ inputToken: ["USDC"] })];
   }
 
   public checkIfUseable(circuit: Circuit): boolean {
